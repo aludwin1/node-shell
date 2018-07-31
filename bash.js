c4a7ff4815deed450ec11e6a49da73c1093ad32d
Aaron Ludwin <aludwin1@binghamton.edu>
2018-07-31 14:32:37 -0400

    cleaned up

--- bash.js
+++ bash.js
@@ -6,6 +6,7 @@ let curl = require('./curl.js');
 
 const done = output => {
     process.stdout.write(output);
+    process.stdout.write('\nprompt >');
 };
 
 
@@ -13,13 +14,10 @@ process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
     const cmds = data.toString().trim().split(' ');
-
     if (cmds[0] === "pwd") pwd(done);
     if (cmds[0] === 'ls') ls(done);
     if (cmds[0] === 'cat') cat(done, cmds[1]);
     if (cmds[0] === 'curl') curl(done, cmds[1]);
-
-    setTimeout(() => { process.stdout.write('\nprompt >') }, 1000)
-})
+});
 
 module.exports = done;
